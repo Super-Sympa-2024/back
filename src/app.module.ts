de6bdb650bc96ea8fa1f9826abc310fbd5fad51c
TangoCharlie <elouanreymond@tutanota.com>
2024-12-05 23:25:53 +0100

    feat: jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, EventModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [appConfig] }),
+    EventModule,
+  ],
 })
 export class AppModule {}
